f38ea88028101a1a5c9afc9afe0776ed3f792cdb
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-08-29 18:01:44 +0530

    ls -l perms

--- builtin.c
+++ builtin.c
@@ -5,9 +5,15 @@
 #include <pwd.h>
 #include <sys/utsname.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/param.h>
+#include <sys/dir.h>  
+#include <sys/stat.h>    
 #include <dirent.h>
+#include <errno.h>
 
 #include "system_details.h"
+
 
 void pwd() {
 
@@ -17,6 +23,7 @@ void pwd() {
 
     return;
 }
+
 
 void cd(char *dest) {
 
@@ -35,6 +42,7 @@ void cd(char *dest) {
     return;
 }
 
+
 void echo(char *token) {
 
     token = strtok(NULL, " \t\n\r");
@@ -50,6 +58,7 @@ void echo(char *token) {
     return;
 }
 
+
 void ls(char *token) {
 
     token = strtok(NULL, " \t\n\r");
@@ -62,7 +71,7 @@ void ls(char *token) {
 
     int i, j, k;
 
-    // Check if token == NULL
+    // CHECK PRESENT DIRECTORY
 
     while(token!=NULL)
     {
@@ -74,7 +83,84 @@ void ls(char *token) {
         token = strtok(NULL, " \t\n\r");
     }
 
-    for(i=0; i<count; ++i) printf("dbug %s\n", dirs[i]);
+    // Iterating through the directories on which we do ls
+    for(i=0; i<count; ++i) 
+    {
+        char dir[1000];
+
+        // Check for home
+        if(dirs[i]==NULL || (strcmp(dirs[i],"~")==0) || (strcmp(dirs[i],"~/")==0)) strcpy(dir, HOME);
+        else if((dirs[i][0]=='~') && (dirs[i][1]=='/')) 
+        {
+            strcpy(dir, HOME);
+            strcat(dir, &(dirs[i])[1]);
+        }
+        else strcpy(dir, dirs[i]);
+
+        // ls on the current directory
+        DIR* cur_dir = opendir(dir);
+        struct dirent *files;
+        struct stat filestat;
+
+        if(cur_dir)
+        {
+            char buf[512];  
+
+            while((files = readdir(cur_dir)) != NULL)
+            {
+                sprintf(buf, "%s/%s", dir, files->d_name);
+                stat(buf, &filestat);
+                if(!l_flag) 
+                {
+                    if(!a_flag)
+                    {
+                        if((files->d_name[0])!='.') printf("%s\n", files->d_name);
+                    }
+                    else printf("%s\n", files->d_name);
+                }
+                else 
+                {
+                    mode_t mode = filestat.st_mode;
+
+                    char filetype = '?';
+                    if(S_ISREG(mode)) filetype = '-';
+                    if(S_ISLNK(mode)) filetype = 'l';
+                    if(S_ISDIR(mode)) filetype = 'd';
+                    if(S_ISBLK(mode)) filetype = 'b';
+                    if(S_ISCHR(mode)) filetype = 'c';
+                    if(S_ISFIFO(mode)) filetype = '|';
+
+                    char ur = '-', uw = '-', ux = '-';
+                    char gr = '-', gw = '-', gx = '-';
+                    char or = '-', ow = '-', ox = '-';
+                    if(mode & S_IRUSR) ur = 'r';
+                    if(mode & S_IWUSR) uw = 'w';
+                    if(mode & S_IXUSR) ux = 'x';
+                    if(mode & S_IRGRP) gr = 'r';
+                    if(mode & S_IWGRP) gw = 'w';
+                    if(mode & S_IXGRP) gx = 'x';
+                    if(mode & S_IROTH) or = 'r';
+                    if(mode & S_IWOTH) ow = 'w';
+                    if(mode & S_IXOTH) ox = 'x';
+
+                    if(!a_flag)
+                    {
+                        if((files->d_name[0])!='.') 
+                            printf("%c%c%c%c%c%c%c%c%c%c\t%s\n", filetype, ur, uw, ux, gr, gw, gx, or, ow, ox, files->d_name);
+                    }
+                    else printf("%c%c%c%c%c%c%c%c%c%c\t%s\n", filetype, ur, uw, ux, gr, gw, gx, or, ow, ox, files->d_name);
+                    
+                }
+
+            }
+            closedir(cur_dir);
+        }
+        else
+        {
+            perror("Error");
+            continue;
+        }
+    }
 
     return;
 }
