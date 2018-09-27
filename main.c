a3c2253c25bd933d90181682f714420133f55140
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-09-27 13:53:10 +0530

    added ctrl d

--- main.c
+++ main.c
@@ -10,33 +10,16 @@
 #include "config.h"
 #include "bg.h"
 
-void ctrlz_handler() { return; }
-
-// static sigjmp_buf env;
-// static volatile sig_atomic_t jump_active = 0;
-
-// void sigint_handler(int signo) {
-//     if (!jump_active) return;
-//     siglongjmp(env, 42);
-// }
-
 int main() {
 
     init();
 
-    // struct sigaction s;
-    // s.sa_handler = sigint_handler;
-    // sigemptyset(&s.sa_mask);
-    // s.sa_flags = SA_RESTART;
-    // sigaction(SIGINT, &s, NULL);
-
-    signal(SIGINT, ctrlz_handler);
-    signal(SIGTSTP, ctrlz_handler);
+    signal(SIGINT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
 
     while(1) 
     {
-        // if(sigsetjmp(env, 1) == 42) printf("\n");
-        // jump_active = 1; 
         check_process();
         display_prompt();
         interpret_commands();
