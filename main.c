a298098cb6b8b2efc78c1cc6e8c0e3e8d0dad80a
Sanjana Sunil <sanjana99sunil@gmail.com>
2018-09-27 23:05:18 +0530

    completed signal termination handling

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ int main() {
 
     signal(SIGINT, SIG_IGN);
     signal(SIGTSTP, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTERM, SIG_IGN);
+    signal(SIGSTOP, SIG_IGN);
 
     while(1) 
     {
